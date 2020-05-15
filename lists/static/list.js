c149d0cdd5a0d18733a97b2931e1b9ae5e35d29e
Christian Simmons <csimmonscs@gmail.com>
2020-05-15 14:59:03 -0700

    Fixed base.py extra space in wait_for_row...

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,9 +1,6 @@
-windows.Superlists = {};
+window.Superlists = {};
 window.Superlists.initialize = function (){
-  // console.log('initialize called')
   $('input[name="text"]').on("keypress", function (){
-    // console.log('in keypress handler');
     $('.has-error').hide();
   });
 };
-// console.log('list.js loaded');
