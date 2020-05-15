959632aec16b6df5c629c070045edb49ff712fb2
Christian Simmons <csimmonscs@gmail.com>
2020-05-15 14:15:11 -0700

    End of chapter 16

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,5 @@
-var initialize = function (){
+windows.Superlists = {};
+window.Superlists.initialize = function (){
   // console.log('initialize called')
   $('input[name="text"]').on("keypress", function (){
     // console.log('in keypress handler');
