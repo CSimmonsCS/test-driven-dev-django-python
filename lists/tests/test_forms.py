fc8c52f7f10e55a596257b1913484c5ddb17b110
Christian Simmons <csimmonscs@gmail.com>
2020-05-14 20:56:20 -0700

    Finished chapter 15

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -27,6 +27,12 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, list_)
 
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
+
 class ExistingListItemFormTest(TestCase):
 
     def test_form_renders_item_test_input(self):
@@ -36,7 +42,7 @@ class ExistingListItemFormTest(TestCase):
 
     def test_form_validation_for_blank_items(self):
         list_ = List.objects.create()
-        form = ExistingListItemForm(for_list=list_)
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
 
